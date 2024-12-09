7d657462660203e16560d46990ebe67009cf8cd3
Reach <reach@42paris.fr>
2024-12-09 02:47:13 +0100

    feat(plugins): add unisonweb/unison

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -28,4 +28,14 @@ return {
   { "hashivim/vim-terraform", ft = "terraform" },
 
   { "pearofducks/ansible-vim", ft = { "yaml", "jinja2" } },
+
+  {
+    "unisonweb/unison",
+    branch = "trunk",
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/editor-support/vim")
+      require("lazy.core.loader").packadd(plugin.dir .. "/editor-support/vim")
+    end,
+    init = function(plugin) require("lazy.core.loader").ftdetect(plugin.dir .. "/editor-support/vim") end,
+  },
 }
